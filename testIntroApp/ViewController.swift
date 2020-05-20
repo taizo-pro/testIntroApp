d43726a3e47a5ef1bfd5782148bb0ae107e4a05b
taizo-pro <t2s.single@gmail.com>
2020-05-20 15:30:23 +0900

    Initial Commit

---
+++ testIntroApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testIntroApp
+//
+//  Created by Kazuki Harada on 2020/05/20.
+//  Copyright © 2020 Harada Kazuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
