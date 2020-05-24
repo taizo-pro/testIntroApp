1e41ea5240e62d68753ddba985bbf9ea1b7bf925
taizo-pro <t2s.single@gmail.com>
2020-05-24 23:23:51 +0900

    XMLパースの実装

--- testIntroApp/Controller/BaseViewController.swift
+++ testIntroApp/Controller/BaseViewController.swift
@@ -7,24 +7,62 @@
 //
 
 import UIKit
+import SegementSlide
 
-class BaseViewController: UIViewController {
+class BaseViewController: SegementSlideDefaultViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        reloadData()
+        defaultSelectedIndex = 0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func segementSlideHeaderView() -> UIView? {
+        //headerに画像をつける
+        let headerView = UIImageView()
+        headerView.isUserInteractionEnabled = true
+        headerView.contentMode = .scaleAspectFill
+        headerView.image = UIImage(named: "header")
+        //オプション
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        //headerの高さ
+        let headerHeight:CGFloat
+        if #available(iOS 11.0, *){
+            headerHeight = view.bounds.height / 4 + view.safeAreaInsets.top
+        }else{
+            headerHeight = view.bounds.height / 4 + topLayoutGuide.length
+        }
+        headerView.heightAnchor.constraint(equalToConstant: headerHeight).isActive = true
+        
+        return headerView
     }
-    */
+
+    //タブの作成
+    //タイトルの作成
+    override var titlesInSwitcher: [String]{
+        return ["TOP","AbemaNews","Yahoo","IT","Buzz","CNN",]
+    }
+    
+    //タブごとにコントローラーを返す（タブの下の各コントローラーを配置する）
+    override func segementSlideContentViewController(at index: Int) -> SegementSlideContentScrollViewDelegate? {
+        switch index {
+        case 0:
+            return Page1ViewController()
+        case 1:
+            return Page2ViewController()
+        case 2:
+            return Page3ViewController()
+        case 3:
+            return Page4ViewController()
+        case 4:
+            return Page5ViewController()
+        case 5:
+            return Page6ViewController()
+        default:
+            return Page1ViewController()
+        }
+    }
+    
 
 }
